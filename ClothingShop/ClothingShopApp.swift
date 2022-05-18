eb4219cee2dc6b7837caa1686ed75ed378936da5
Janessa Vazquez <61309969+janessa17@users.noreply.github.com>
2022-05-17 18:24:25 -0700

    first

---
+++ ClothingShop/ClothingShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingShopApp.swift
+//  ClothingShop
+//
+//  Created by Janessa Vazquez on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ClothingShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
