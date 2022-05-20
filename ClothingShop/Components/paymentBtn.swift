91615ec1eff46d1d905a77cac5275cd401de06bd
Janessa Vazquez <61309969+janessa17@users.noreply.github.com>
2022-05-19 21:57:29 -0700

    Implemented payment button

---
+++ ClothingShop/Components/paymentBtn.swift
@@ -0,0 +1,51 @@
+//
+//  paymentBtn.swift
+//  ClothingShop
+//
+//  Created by Janessa Vazquez on 5/19/22.
+//
+
+import SwiftUI
+import PassKit
+struct paymentBtn: View {
+    var action: () -> Void
+    var body: some View {
+      Representable(action: action)
+            .frame(minWidth: 100, maxWidth: 400)
+            .frame(height: 45)
+            .frame(maxWidth: .infinity)
+    }
+}
+
+struct paymentBtn_Previews: PreviewProvider {
+    static var previews: some View {
+        paymentBtn(action: {})
+    }
+}
+extension paymentBtn {
+    struct Representable: UIViewRepresentable {
+        var action: () -> Void
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator(action: action)
+        }
+        func makeUIView(context: Context) -> some UIView {
+            context.coordinator.button
+        }
+        func updateUIView(_ uiView: UIViewType, context: Context) {
+            context.coordinator.action = action
+        }
+    }
+    class Coordinator: NSObject {
+        var action: () -> Void
+        var button = PKPaymentButton(paymentButtonType: .buy, paymentButtonStyle: .automatic)
+        init(action: @escaping () -> Void) {
+            self.action = action
+            super.init()
+            button.addTarget(self, action: #selector(callback(_:)), for: .touchUpInside)
+        }
+        @objc func callback(_ sender: Any) {
+            action()
+        }
+    }
+}
